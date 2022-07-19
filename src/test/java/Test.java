bee9959b8b051164ec8b609e5e3ffb7400c592e3
A_Nan <2358325523@qq.com>
2022-07-19 15:33:50 +0800

    feat：添加聚合、IPv4前缀查询、IPv6查询、正则查询（暂无ip、正则正确性校验）

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,7 +2,8 @@ import com.power.es.AIQL.Utils;
 import org.elasticsearch.index.query.QueryBuilders;
 
 public class Test {
-    public static void main(String[] args) {
+    @org.junit.Test
+    public void commonTest(String[] args) {
         System.out.println(Utils.getEsQuery("! (a == 1 OR b == 4) OR c==3 AND d == 5"));
     }
 }
